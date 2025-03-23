3e96551d6c650387b015e4d2c2eaa5a0b534e521
teodelcastillo <teodorodelcastillop@gmail.com>
2025-03-23 12:20:34 -0300

    Fixing navbar

--- app/protected/components/layout/header.tsx
+++ app/protected/components/layout/header.tsx
@@ -5,17 +5,22 @@ import { Separator } from "@/components/ui/separator"
 import { UserNav } from "./user-nav"
 import { ModeToggle } from "./mode-toggle"
 
-
 export function Header() {
   return (
-    <header className="sticky top-0 z-10 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
-      <SidebarTrigger />
-      <Separator orientation="vertical" className="h-6" />
-      <div className="w-full flex items-center justify-between">
-        <div className="flex items-center gap-2">
-          <ModeToggle />
-          <UserNav />
-        </div>
+    <header className="sticky top-0 z-10 flex h-16 items-center border-b bg-background px-4 md:px-6">
+      {/* LEFT SIDE: Sidebar Trigger */}
+      <div className="flex items-center gap-4">
+        <SidebarTrigger />
+        <Separator orientation="vertical" className="h-6" />
+      </div>
+
+      {/* CENTER (Empty space grows) */}
+      <div className="flex-1" />
+
+      {/* RIGHT SIDE: Mode Toggle + UserNav */}
+      <div className="flex items-center gap-2">
+        <ModeToggle />
+        <UserNav />
       </div>
     </header>
   )
