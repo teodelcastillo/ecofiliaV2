bd4e5c68600682240b087e48a0c587688800dcf8
teodelcastillo <teodorodelcastillop@gmail.com>
2025-04-15 18:29:44 -0300

    Fixing deployment error

--- app/actions/sign-up.ts
+++ app/actions/sign-up.ts
@@ -3,7 +3,6 @@
 import { encodedRedirect } from "@/utils/utils";
 import { createClient } from "@/utils/supabase/server";
 import { headers } from "next/headers";
-import { redirect } from "next/navigation";
 
 export const signUpAction = async (formData: FormData) => {
   const fullName = formData.get("full_name")?.toString();
