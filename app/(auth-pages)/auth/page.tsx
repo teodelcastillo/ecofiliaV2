1496e536245ec31f7de6fe8c0a338bae6384ed57
teodelcastillo <teodorodelcastillop@gmail.com>
2025-05-12 18:29:21 -0300

    Fixing for deployment

--- app/(auth-pages)/auth/page.tsx
+++ app/(auth-pages)/auth/page.tsx
@@ -1,6 +1,11 @@
 import { signUpAction } from "@/app/actions/sign-up";
 import { AuthClient } from "./auth-client";
+import { Suspense } from "react";
 
 export default function AuthPage() {
-  return <AuthClient onSignUp={signUpAction} />;
+  return (
+  <Suspense fallback={<div className="p-8 text-center">Loading...</div>}>
+
+   <AuthClient onSignUp={signUpAction} />
+  </Suspense>)
 }
